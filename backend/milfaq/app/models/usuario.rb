4f4bd7ce0abf935f0c42ce989720097de0528f8b
Joaquim Pedro <pedro.quindin@gmail.com>
2015-03-20 14:00:53 -0300

    #12 Obrigatoriedade de nome, sobrenome, email e perfil - #13 Validacao de email unico

--- backend/milfaq/app/models/usuario.rb
+++ backend/milfaq/app/models/usuario.rb
@@ -1,3 +1,8 @@
 class Usuario < ActiveRecord::Base
 	belongs_to :perfil
+
+	validates :nome, :presence => {message: "Nome é obrigatório"}
+	validates :sobrenome, :presence => {message: "Sobrenome é obrigatório"}
+	validates :email, :presence => {message: "E-mail é obrigatório"}, :uniqueness => {message: "E-mail já registrado"}
+	validates :perfil_id, :presence => {message: "Perfil é obrigatório"}
 end
