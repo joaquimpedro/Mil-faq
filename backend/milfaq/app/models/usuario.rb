3b3d998be5a8a3efd0530b0df0cce3cbacc0f443
Joaquim Pedro <pedro.quindin@gmail.com>
2015-03-21 14:09:30 -0300

    #73 - Unicidade do email com case-sensitive

--- backend/milfaq/app/models/usuario.rb
+++ backend/milfaq/app/models/usuario.rb
@@ -3,7 +3,7 @@ class Usuario < ActiveRecord::Base
 
 	validates :nome, :presence => {message: "Nome é obrigatório"}
 	validates :sobrenome, :presence => {message: "Sobrenome é obrigatório"}
-	validates :email, :presence => {message: "E-mail é obrigatório"}, :uniqueness => {message: "E-mail já registrado"}
+	validates :email, :presence => {message: "E-mail é obrigatório"}, :uniqueness => {message: "E-mail já registrado", case_sensitive: false}
 	validates :perfil_id, :presence => {message: "Perfil é obrigatório"}
 
 	def nome_completo
